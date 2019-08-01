ea611f3c64616c092580851a967cce36a9f849a1
lvtu0316 <550407948@qq.com>
2019-08-01 15:11:37 +0800

    增加白名单

--- config/default.py
+++ config/default.py
@@ -57,7 +57,12 @@ MIDDLEWARE += (
 # 跨域增加忽略
 CORS_ALLOW_CREDENTIALS = True
 CORS_ORIGIN_ALLOW_ALL = True
-CORS_ORIGIN_WHITELIST = ( )
+CORS_ORIGIN_WHITELIST = (
+    'http://127.0.0.1:8080',
+    'http://localhost:8080',
+     'http://localhost:80',
+     'http://127.0.0.1:80',
+)
 
 CORS_ALLOW_METHODS = (
     'DELETE',
